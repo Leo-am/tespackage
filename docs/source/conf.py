6f3a7abbd2dd75a2e7d44088e4e54350a25b79c7
Leonardo Assis <39472087+Leo-am@users.noreply.github.com>
2021-09-24 12:17:51 +1000

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,12 +2,12 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'tes package'
+copyright = '2021, Leonardo'
+author = 'Leonardo Assis'
 
-release = '0.1'
-version = '0.1.0'
+release = '0.2'
+version = '0.2.0'
 
 # -- General configuration
 
