5bc5dc975b7b1159ca470e34af25ac46dca79c5d
Leonardo Assis <39472087+Leo-am@users.noreply.github.com>
2021-09-25 09:47:35 +1000

    fixing address

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -53,6 +53,7 @@ import os
 import sys
 
 #sys.path.insert(0, os.path.abspath('/Users/leo/TES_package_and_data/Useful Jupyter Notebooks/GitHub/tespackage'))
+sys.path.insert(0, os.path.abspath('../../tes/'))
 sys.path.insert(0, os.path.abspath('_ext'))
 
 # -- Options for HTML output -------------------------------------------------
