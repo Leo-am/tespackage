75e9b5ffa3dc5753d0b1bd99d5d935c07455749a
Leonardo Assis <39472087+Leo-am@users.noreply.github.com>
2021-09-25 11:29:16 +1000

    add mock modules

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -61,3 +61,10 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+
+import mock
+ 
+MOCK_MODULES = ['numpy', 'matplotlib', 'numba', 'scipy', 'lmfit', 'bokeh']
+for mod_name in MOCK_MODULES:
+sys.modules[mod_name] = mock.Mock()
